caf1939139ed4b04e3ef5dc0316c0a749956a77e
AleksejRonzhin <aleksej.ronzhin.rsreu@gmail.com>
2022-04-06 14:37:21 +0300

    Finished lab 4

--- lab4-dynamic-table/dynamic-table.js
+++ lab4-dynamic-table/dynamic-table.js
@@ -1,9 +1,8 @@
 const red = "#dc143c";
 const blue = "#0fffff";
-
-function fillRedCells(value) {
-    console.log(value);
-}
+const empty = "transparent";
+let lastArray;
+let currentRotate = 0;
 
 function generateTableDiv() {
     let tableDiv = document.createElement("div");
@@ -30,8 +29,8 @@ function tryConstructTable(size) {
         report(isCorrectCheckResult.message);
         return;
     }
-
     constructTable(size);
+    updateTableBody();
 }
 
 function report(message) {
@@ -45,11 +44,15 @@ function report(message) {
 function constructTable(size) {
     let tableDiv = document.getElementById("tableDiv");
     let table = document.createElement("table");
+    table.id = "table";
     let tableBody = document.createElement("tbody");
+    tableBody.id = "tableBody";
     table.appendChild(tableBody);
     let array = generateArray(size);
+    lastArray = array.slice();
     generateTableCells(tableBody, array);
     tableDiv.appendChild(table);
+    lastArray = array;
 }
 
 function generateArray(size) {
@@ -57,7 +60,7 @@ function generateArray(size) {
     for (let i = 0; i < size; i++) {
         let row = Array(size);
         row.fill({
-            value: "", color: "transparent"
+            value: "", color: empty
         })
         array.unshift(row);
     }
@@ -78,7 +81,7 @@ function fillColumns(array) {
     fillVectors(array, fillColumn);
 }
 
-function fillVectors(array, fillVector){
+function fillVectors(array, fillVector) {
     let size = array.length;
     let values = Array(size);
     for (let i = 0; i < size; i++) {
@@ -125,18 +128,20 @@ function generateTableCells(tableBody, array) {
 
 function isIntegerSize(size) {
     if (!isNumeric(size)) {
-        console.log("не число");
         return {
             result: false, message: "Введено не число"
         }
     }
-    if (size % 1 !== 0) {
-        console.log("не целое");
+    if (!isInteger(size)) {
         return {
             result: false, message: "Введено не целое число"
         }
     }
     return {result: true};
+}
+
+function isInteger(value){
+    return (value % 1 === 0);
 }
 
 function isNumeric(value) {
@@ -155,4 +160,113 @@ function isCorrectSize(size) {
         }
     }
     return {result: true};
+}
+
+// Наложение эффектов
+function updateTableBody() {
+    let tableBody = document.getElementById("tableBody");
+    tableBody.innerHTML = "";
+    generateTableCells(tableBody, lastArray);
+    applyEffects();
+}
+
+function applyEffects(){
+    tryRemoveRowsColor();
+    tryFillRedCells();
+}
+
+function tryRemoveRowsColor(){
+    let input = document.getElementById("removeRowsColorInput");
+    let rows = getSelectedRows(input.value);
+    removeRowsColor(rows);
+}
+
+function getSelectedRows(value) {
+    let rows = value.split(",");
+    rows = rows.map(num => parseFloat(num));
+    rows = rows.filter(isInteger);
+    return rows;
+}
+
+function removeRowsColor(removeColorRows) {
+    let rows = document.getElementById("tableDiv").getElementsByTagName("tr");
+    for (let r = 0; r < removeColorRows.length; r++) {
+        for (let i = 0; i < rows.length; ++i) {
+            if (i === removeColorRows[r]) {
+                let cells = rows[i].getElementsByTagName("td");
+                for (let j = 0; j < cells.length; ++j) {
+                    cells[j].bgColor = empty;
+                }
+            }
+        }
+    }
+}
+
+function tryFillRedCells(){
+    let form = document.getElementById("fillRedCellsForm");
+    if (form.radio.value !== "") {
+        fillRedCells(form.radio.value);
+    }
+}
+
+function fillRedCells(aggregate) {
+    let cells = document.getElementById("tableDiv").getElementsByTagName("td");
+    let value = getValue(aggregate);
+    for (let i = 0; i < cells.length; ++i) {
+        if (cells[i].bgColor === red) {
+            cells[i].textContent = value;
+        }
+    }
+}
+
+function getValue(aggregate) {
+    if (aggregate === "sum") {
+        return getSum();
+    }
+    return getAvg();
+}
+
+function getSum() {
+    let sum = 0;
+    for (let i = 0; i < lastArray.length; i++) {
+        for (let j = 0; j < lastArray[i].length; j++) {
+            let cell = lastArray[i][j];
+            if (cell.color !== empty) {
+                sum += cell.value;
+            }
+        }
+    }
+    return sum;
+}
+
+function getCount() {
+    let count = 0;
+    for (let i = 0; i < lastArray.length; i++) {
+        for (let j = 0; j < lastArray[i].length; j++) {
+            let cell = lastArray[i][j];
+            if (cell.color !== empty) {
+                count += 1;
+            }
+        }
+    }
+    return count;
+}
+
+function getAvg() {
+    let sum = getSum();
+    let count = getCount();
+    return sum / count;
+}
+
+function resetRedCells() {
+    let form = document.getElementById("fillRedCellsForm");
+    form.reset();
+    updateTableBody();
+}
+
+function rotate(){
+    let table = document.getElementById("table");
+    currentRotate += 90;
+    currentRotate %= 360;
+    table.style.transform = "rotate(" + currentRotate + "deg)";
 }
